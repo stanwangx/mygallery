2eec87373a7fd5f0873592e7f82d5bfc6027bcdf
stan <momos.wang@gmail.com>
2019-01-13 22:12:57 +0800

    更新数据库，建立图片存储。等等一系列事情。

--- gallery/models.py
+++ gallery/models.py
@@ -5,5 +5,10 @@ from django.db import models
 # 创建app，使用 python manage.py startapp
 
 class Gallery(models.Model):
-		descrption = models.CharField(default='这里是默认描述'，max_length=100)
-    	image = models.ImageField(default='default.png', upload_to='/images/')
+		descrption = models.CharField(default='这里是默认描述', max_length=100)
+		gimage = models.ImageField(default='default.png', upload_to='gimages/')
+		gtitle = models.CharField(default='作品标题', max_length=50)
+
+		def __str__(self):
+			return self.gtitle
+    
